bb181a23167506bc98236e80fc9ae0fb95b9553e
PatrickMessierGoC <45016166+PatrickMessierGoC@users.noreply.github.com>
2020-06-22 09:10:56 -0400

    Issue 91 (#5)
    * Update ARM templates based on comments received by ITSysAdmin: rename resources, add firewall rules.
    * Refactor parameter handling.
    * Prevent script from running when a subcommand fails.
    * Update ReadMe.
    Co-authored-by: Patrick Messier <Patrick.Messier@052gc.onmicrosoft.com>

--- scripts/create_vm_image.sh
+++ scripts/create_vm_image.sh
@@ -1,120 +1,148 @@
 #!/bin/bash
 
-################################################################################
-echo 'Mapping input parameters...'
-databaseAdminPassword="$1"
-databaseAdminUsername="$2"
-databaseMoodlePassword="$3"
-databaseMoodleUsername="$4"
-fileRepositoryUri="$5"
-gateway_publicIp_fqdn="$6"
-moodleAdminEmail="$7"
-moodleAdminPassword="$8"
-moodleAdminUsername="$9"
-shift 9
-moodle_share_name="$1"
-moodle_storageAccount_key="$2"
-moodle_storageAccount_name="$3"
-paz_networkSecurityGroup_name="$4"
-paz_subnet_name="$5"
-postgresql_moodleDb_name="$6"
-postgresql_name="$7"
-redis_name="$8"
-redis_primarykey="$9"
-shift 9
-resourceGroup="$1"
-virtualNetwork_name="$2"
-webServerAdminPassword="$3"
-webServerAdminUsername="$4"
-vmName="$5"
-vmImageName="$6"
-moodleUpgradeKey="$7"
+# exit when any command fails
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 ################################################################################
-echo 'Echo values for debug purposes...'
-echo "databaseAdminPassword = $databaseAdminPassword"
-echo "databaseAdminUsername = $databaseAdminUsername"
-echo "databaseMoodlePassword = $databaseMoodlePassword"
-echo "databaseMoodleUsername = $databaseMoodleUsername"
-echo "fileRepositoryUri = $fileRepositoryUri"
-echo "gateway_publicIp_fqdn = $gateway_publicIp_fqdn"
-echo "moodleAdminEmail = $moodleAdminEmail"
-echo "moodleAdminPassword=$moodleAdminPassword"
-echo "moodleAdminUsername = $moodleAdminUsername"
-echo "moodle_share_name = $moodle_share_name"
-echo "moodle_storageAccount_key = $moodle_storageAccount_key"
-echo "moodle_storageAccount_name = $moodle_storageAccount_name"
-echo "paz_networkSecurityGroup_name = $paz_networkSecurityGroup_name"
-echo "paz_subnet_name = $paz_subnet_name"
-echo "postgresql_moodleDb_name = $postgresql_moodleDb_name"
-echo "postgresql_name = $postgresql_name"
-echo "redis_name = $redis_name"
-echo "redis_primarykey = $redis_primarykey"
-echo "resourceGroup = $resourceGroup"
-echo "virtualNetwork_name = $virtualNetwork_name"
-echo "webServerAdminPassword = $webServerAdminPassword"
-echo "webServerAdminUsername = $webServerAdminUsername"
-echo "vmName = $vmName"
-echo "vmImageName =$vmImageName"
-echo "moodleUpgradeKey = $moodleUpgradeKey"
+echo 'Initialize expected parameters array...'
+declare -A parameters=( [applicationNetworkSecurityGroupName]= \
+                        [applicationSubnetName]= \
+                        [databaseAdminPassword]= \
+                        [databaseAdminUsername]= \
+                        [databaseApplicationDatabaseName]= \
+                        [databaseMoodlePassword]= \
+                        [databaseMoodleUsername]= \
+                        [databaseFqdn]= \
+                        [databaseName]= \
+                        [fileRepositoryUri]= \
+                        [gatewayPublicIpFqdn]= \
+                        [moodleAdminEmail]= \
+                        [moodleAdminPassword]= \
+                        [moodleAdminUsername]= \
+                        [moodleShareName]= \
+                        [moodleStorageAccountFilePrimaryEndPoint]= \
+                        [moodleStorageAccountKey]= \
+                        [moodleStorageAccountName]= \
+                        [moodleUpgradeKey]= \
+                        [redisHostName]= \
+                        [redisName]= \
+                        [redisPrimaryKey]= \
+                        [resourceGroupName]= \
+                        [virtualNetworkName]= \
+                        [vmImageName]= \
+                        [vmName]= \
+                        [webServerAdminPassword]= \
+                        [webServerAdminUsername]= )
+
+sortedParameterList=$(echo ${!parameters[@]} | tr " " "\n" | sort | tr "\n" " ");
+
+echo "Mapping input parameter values and checking for extra parameters..."
+while [[ ${#@} -gt 0 ]];
+do
+    key=$1
+    value=$2
+
+    ## Test if the parameter key start with "-" and if the parameter key (without the first dash) is in the expected parameter list.
+    if [[ ${key} =~ ^-.*$ && ${parameters[${key:1}]+_} ]]; then
+        parameters[${key:1}]="$value"
+    else
+        echo "ERROR: Unexpected parameter: $key"
+        extraParameterFlag=true;
+    fi
+
+    # Move to the next key/value pair or up to the end of the parameter list.
+    shift $(( 2 < ${#@} ? 2 : ${#@} ))
+done
+
+echo "Checking for missing parameters..."
+for p in $sortedParameterList; do
+    if [[ -z ${parameters[$p]} ]]; then
+        echo "ERROR: Missing parameter: $p."
+        missingParameterFlag=true;
+    fi
+done
+
+if [[ -z $extraParameterFlag && -z $missingParameterFlag ]]; then
+    echo "INFO: No missing or extra parameters."
+else
+    echo "ERROR: Execution aborted due to missing or extra parameters."
+    usage="USAGE: $(basename $0)"
+    for p in $sortedParameterList; do
+        usage="${usage} -${p} \$${p}"
+    done
+    echo "${usage}";
+    exit 1;
+fi
+
+echo 'Echo parameter values for debug purpose...'
+for p in $sortedParameterList; do
+    echo "DEBUG: $p = \"${parameters[$p]}\""
+done
 
 ################################################################################
 echo "Creating VM ${vmName}..."
 az vm create \
-    --name $vmName \
-    --resource-group $resourceGroup \
+    --name "${parameters[vmName]}" \
+    --resource-group "${parameters[resourceGroupName]}" \
     --size "Standard_D2s_v3" \
     --storage-sku "Premium_LRS" \
     --image "Canonical:UbuntuServer:18.04-LTS:latest" \
     --computer-name "webserver" \
-    --admin-username "$webServerAdminUsername" \
-    --admin-password "$webServerAdminPassword" \
-    --subnet "$paz_subnet_name" \
-    --vnet-name "$virtualNetwork_name" \
-    --nsg "$paz_networkSecurityGroup_name" \
+    --admin-username "${parameters[webServerAdminUsername]}" \
+    --admin-password "${parameters[webServerAdminPassword]}" \
+    --subnet "${parameters[applicationSubnetName]}" \
+    --vnet-name "${parameters[virtualNetworkName]}" \
+    --nsg "${parameters[applicationNetworkSecurityGroupName]}" \
     --public-ip-address ""
 
 ################################################################################
 echo "Running VM extension to install moodle..."
 az vm extension set \
-    --resource-group $resourceGroup \
-    --vm-name $vmName \
+    --resource-group "${parameters[resourceGroupName]}" \
+    --vm-name "${parameters[vmName]}" \
     --name "CustomScript" \
     --publisher "Microsoft.Azure.Extensions" \
     --version "2.1" \
     --settings \
         "{ \
             \"fileUris\": [ \
-                \"$fileRepositoryUri/scripts/install_moodle.sh\", \
-                \"$fileRepositoryUri/scripts/update_muc.php\" \
+                \"${parameters[fileRepositoryUri]}/scripts/install_moodle.sh\", \
+                \"${parameters[fileRepositoryUri]}/scripts/update_muc.php\" \
             ] \
         }" \
     --protected-settings \
         "{ \
             \"commandToExecute\": \"sudo ./install_moodle.sh \
-                $moodle_storageAccount_name \
-                $moodle_storageAccount_key \
-                $moodle_share_name \
-                $postgresql_name \
-                $databaseAdminUsername \
-                $databaseAdminPassword \
-                $postgresql_moodleDb_name \
-                $databaseMoodleUsername \
-                $databaseMoodlePassword \
-                $gateway_publicIp_fqdn \
-                $moodleAdminUsername \
-                $moodleAdminPassword \
-                $moodleAdminEmail \
-                $redis_name \
-                $redis_primarykey \
-                $moodleUpgradeKey > /var/log/install_moodle.log\" \
+                -dbServerAdminPassword ${parameters[databaseAdminPassword]} \
+                -dbServerAdminUsername ${parameters[databaseAdminUsername]} \
+                -dbServerFqdn ${parameters[databaseFqdn]} \
+                -dbServerName ${parameters[databaseName]} \
+                -fileShareName ${parameters[moodleShareName]} \
+                -moodleAdminEmail ${parameters[moodleAdminEmail]} \
+                -moodleAdminPassword ${parameters[moodleAdminPassword]} \
+                -moodleAdminUsername ${parameters[moodleAdminUsername]} \
+                -moodleDbName ${parameters[databaseApplicationDatabaseName]} \
+                -moodleDbPassword ${parameters[databaseMoodlePassword]} \
+                -moodleDbUsername ${parameters[databaseMoodleUsername]} \
+                -moodleFqdn ${parameters[gatewayPublicIpFqdn]} \
+                -moodleUpgradeKey ${parameters[moodleUpgradeKey]} \
+                -redisHostName ${parameters[redisHostName]} \
+                -redisName ${parameters[redisName]} \
+                -redisPrimaryKey ${parameters[redisPrimaryKey]} \
+                -storageAccountEndPoint ${parameters[moodleStorageAccountFilePrimaryEndPoint]} \
+                -storageAccountKey ${parameters[moodleStorageAccountKey]} \
+                -storageAccountName ${parameters[moodleStorageAccountName]} > /var/log/install_moodle.log 2>&1\" \
          }"
 
 ################################################################################
 echo "Scheduling VM deprovisioning process..."
 az vm run-command invoke \
-    --resource-group $resourceGroup \
-    --name $vmName \
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name "${parameters[vmName]}" \
     --command-id RunShellScript \
     --scripts 'echo "waagent -deprovision+user -force" | at now + 1 minutes'
 
@@ -123,42 +151,42 @@ echo "Waiting for the deprovisioning process to finish..."
 sleep 90
 
 ################################################################################
-echo "Deallocating VM ${vmName}..."
+echo "Deallocating VM ${parameters[vmName]}..."
 az vm deallocate \
-    --resource-group $resourceGroup \
-    --name $vmName
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name "${parameters[vmName]}"
 
 ################################################################################
-echo "Generalizing VM ${vmName}..."
+echo "Generalizing VM ${parameters[vmName]}..."
 az vm generalize \
-    --resource-group $resourceGroup \
-    --name $vmName
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name "${parameters[vmName]}"
 
 ################################################################################
-echo "Creating VM image ${vmImageName}..."
+echo "Creating VM image ${parameters[vmImageName]}..."
 az image create \
-    --resource-group $resourceGroup \
-    --name "$vmImageName" \
-    --source $vmName
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name "${parameters[vmImageName]}" \
+    --source "${parameters[vmName]}"
 
 ################################################################################
-echo "Deleting VM ${vmName}..."
+echo "Deleting VM ${parameters[vmName]}..."
 az vm delete \
-    --resource-group $resourceGroup \
-    --name $vmName \
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name "${parameters[vmName]}" \
     --yes
 
 ################################################################################
 echo "Deleting VM Network Interface Card..."
 az network nic delete \
-    --resource-group $resourceGroup \
-    --name $(az network nic list --resource-group $resourceGroup --query "[?contains(name, '$vmName')].name | [0]" -o tsv) \
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name $(az network nic list --resource-group "${parameters[resourceGroupName]}" --query "[?contains(name, '${parameters[vmName]}')].name | [0]" -o tsv) \
 
 ################################################################################
 echo "Deleting VM disk..."
 az disk delete \
-    --resource-group $resourceGroup \
-    --name  $(az disk list --resource-group $resourceGroup --query "[?contains(name, '$vmName')].name | [0]" -o tsv) \
+    --resource-group "${parameters[resourceGroupName]}" \
+    --name  $(az disk list --resource-group "${parameters[resourceGroupName]}" --query "[?contains(name, '${parameters[vmName]}')].name | [0]" -o tsv) \
     --yes
 
 ################################################################################
--- scripts/initialize_vmss_instance.sh
+++ scripts/initialize_vmss_instance.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # This script must be run as root (ex.: sudo sh [script_name])
 
+# exit when any command fails
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
 function echo_title {
     echo ""
     echo "###############################################################################"
@@ -21,7 +28,6 @@ fileShareName="$1"
 echo_title "Recreate moodledata mount point."
 ###############################################################################
 mkdir /mnt/${fileShareName}
-chmod 777 /mnt/${fileShareName}
 
 ###############################################################################
 echo_title "Mount all storages."
